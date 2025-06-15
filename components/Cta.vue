23ee9058a94048a3a26072490020f9e8d31d4910
Victor <victor@gbtsbuildingmaterials.com>
2025-06-15 20:56:50 +0100

    Update Cta.vue
    Changed py-16 to 4 and 8 to 3

--- components/Cta.vue
+++ components/Cta.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 });
 </script>
 <template>
-  <section class="text-center py-16 px-4 bg-white">
+  <section class="text-center py-4 px-4 bg-white">
     <div>
       <h1 class="text-5xl font-extrabold" style="color: #6e8efb">GBTS</h1>
       <p class="mt-2 text-lg text-gray-800">Your One-Stop-Shop For Building Materials</p>
@@ -17,7 +17,7 @@ const props = defineProps({
     <div class="mt-8">
       <NuxtLink
         to="/products"
-        class="inline-block px-8 py-3 bg-[#6e8efb] text-white rounded-lg shadow-md hover:bg-blue-700 transition"
+        class="inline-block px-3 py-3 bg-[#6e8efb] text-white rounded-lg shadow-md hover:bg-blue-700 transition"
       >
         Shop Now
       </NuxtLink>
